4485f47d659afa155be53e7302f400df5a797aaf
Hleb Valoshka <375GNU@Gmail.COM>
2013-09-15 20:12:15 +0300

    Revert "Fix wrong environment variable check"
    This reverts commit 6ec43e971a9f9afa384d7984ca01d92e668902fe.

--- lib/locale/driver/env.rb
+++ lib/locale/driver/env.rb
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 #
 # Copyright (C) 2012  Kouhei Sutou <kou@clear-code.com>
-# Copyright (C) 2012  Hleb Valoshka
 # Copyright (C) 2008  Masao Mutoh
 #
 # Original: Ruby-GetText-Package-1.92.0.
@@ -34,11 +33,11 @@ module Locale
     module Env
       module_function
 
-      # Gets the locale from environment variable. (LC_ALL > LC_CTYPES > LANG)
+      # Gets the locale from environment variable. (LC_ALL > LC_MESSAGES > LANG)
       # Returns: the locale as Locale::Tag::Posix.
       def locale
         # At least one environment valiables should be set on *nix system.
-        [ENV["LC_ALL"], ENV["LC_CTYPES"], ENV["LANG"]].each do |loc|
+        [ENV["LC_ALL"], ENV["LC_MESSAGES"], ENV["LANG"]].each do |loc|
           if loc != nil and loc.size > 0
             return Locale::Tag::Posix.parse(loc)
           end
@@ -46,7 +45,7 @@ module Locale
         nil
       end
 
-      # Gets the locales from environment variables. (LANGUAGE > LC_ALL > LC_CTYPES > LANG)
+      # Gets the locales from environment variables. (LANGUAGE > LC_ALL > LC_MESSAGES > LANG)
       # * Returns: an Array of the locale as Locale::Tag::Posix or nil.
       def locales
         locales = ENV["LANGUAGE"]
--- test/test_detect_general.rb
+++ test/test_detect_general.rb
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 #
 # Copyright (C) 2012  Kouhei Sutou <kou@clear-code.com>
-# Copyright (C) 2012  Hleb Valoshka
 # Copyright (C) 2009-2010  Masao Mutoh
 #
 # License: Ruby's or LGPL
@@ -28,14 +27,14 @@ class TestDetectGeneral < Test::Unit::TestCase
     Locale.init
     Locale.clear_all
     ENV["LC_ALL"] = nil
-    ENV["LC_CTYPES"] = nil
+    ENV["LC_MESSAGES"] = nil
     ENV["LANG"] = nil
     ENV["LANGUAGE"] = nil
   end
 
   def test_lc_all
     ENV["LC_ALL"] = "ja_JP.eucJP"
-    ENV["LC_CTYPES"] = "zh_CN.UTF-8"  #Ignored.
+    ENV["LC_MESSAGES"] = "zh_CN.UTF-8"  #Ignored.
     ENV["LANG"] = "ko_KR.UTF-8"  #Ignored.
     ENV["LANGUAGE"] = nil
 
@@ -51,7 +50,7 @@ class TestDetectGeneral < Test::Unit::TestCase
 
   def test_lc_messages
     ENV["LC_ALL"] = nil
-    ENV["LC_CTYPES"] = "ja_JP.eucJP"
+    ENV["LC_MESSAGES"] = "ja_JP.eucJP"
     ENV["LANG"] = "ko_KR.UTF-8"  #Ignored.
     ENV["LANGUAGE"] = nil
 
@@ -67,7 +66,7 @@ class TestDetectGeneral < Test::Unit::TestCase
 
   def test_lang
     ENV["LC_ALL"] = nil
-    ENV["LC_CTYPES"] = nil
+    ENV["LC_MESSAGES"] = nil
     ENV["LANG"] = "ja_JP.eucJP"
     ENV["LANGUAGE"] = nil
 
@@ -83,7 +82,7 @@ class TestDetectGeneral < Test::Unit::TestCase
 
   def test_lang_complex
     ENV["LC_ALL"] = "zh_CN.UTF-8"  # Ignored.
-    ENV["LC_CTYPES"] = "ko_KR.UTF-8" #Ingored.
+    ENV["LC_MESSAGES"] = "ko_KR.UTF-8" #Ingored.
     ENV["LANG"] = "en_US.UTF-8"  # Ignored.
     ENV["LANGUAGE"] ="ja_JP.eucJP:zh_CN.UTF-8"
 
